d88b0ae85733800278edb7433ddfa8595fa549f2
Robert Runge <robertboerunge@hotmail.com>
2014-05-11 16:26:51 +0200

    -

--- wwwroot/Website/Views/ResourcesLabels.Designer.cs
+++ wwwroot/Website/Views/ResourcesLabels.Designer.cs
@@ -22,14 +22,14 @@ namespace PhonyClubDenmark.Website.Views {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class ResourceLabels {
+    public class ResourcesLabels {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ResourceLabels() {
+        internal ResourcesLabels() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace PhonyClubDenmark.Website.Views {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PhonyClubDenmark.Website.Views.ResourceLabels", typeof(ResourceLabels).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PhonyClubDenmark.Website.Views.ResourcesLabels", typeof(ResourcesLabels).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

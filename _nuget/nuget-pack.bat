5664e56fcb9b7dcbdb965581f153a11f8d746b7f
Robert Runge <robertboerunge@hotmail.com>
2014-11-26 10:34:57 +0100

    nuget

--- _nuget/nuget-pack.bat
+++ _nuget/nuget-pack.bat
@@ -7,6 +7,8 @@ set framework=Net45
 @echo 1. Clean old folder
 @echo ------------------------------------------------------------------------
 @del content\*.* /S /F /Q > nul
+@rmdir /S /Q content
+@mkdir content
 
 echo.
 echo.
@@ -14,10 +16,10 @@ echo.
 @echo ------------------------------------------------------------------------
 @echo 2. Copy files to NuGet folder
 @echo ------------------------------------------------------------------------
-@xcopy web.config.* content\%framework%\
 @xcopy ..\wwwroot\website\*.* content\%framework%\*.* /S /Q /EXCLUDE:dontcopy.txt
 @xcopy ..\wwwroot\website\content\c*.* content\%framework%\Content\*.* /Q
 @xcopy ..\wwwroot\website\scripts\docs.min.js content\%framework%\scripts\*.* /Q
+@xcopy copy-content\*.* content\%framework%\*.* /s /Q
 
 echo.
 echo.
@@ -25,6 +27,12 @@ echo.
 @for /R %%x in (*.cs) do ren "%%x" *.cs.pp
 @for /R %%x in (*.cshtml) do ren "%%x" *.cshtml.pp
 @FORFILES /P "Content" /M *.pp /S /C "Cmd /C %replacetext% @path PhonyClubDenmark.Website $rootnamespace$ /I"
+
+@echo ------------------------------------------------------------------------
+@echo Target .NET 4.5.1 + 4.5.2
+@echo ------------------------------------------------------------------------
+@xcopy content\Net45\*.* content\Net451\ /s /q 
+@xcopy content\Net45\*.* content\Net452\ /s /q 
 
 @echo ------------------------------------------------------------------------
 @echo 3. Pack it all up
@@ -36,5 +44,5 @@ echo.
 @echo ------------------------------------------------------------------------
 @echo DONE
 @echo ------------------------------------------------------------------------
-pause
 :end
+pause
